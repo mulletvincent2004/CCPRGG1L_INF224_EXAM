8edc200a2c6a59be276389530ff0ca55a5f2f694
Mullet, Vincent Jay R <111546395+mulletvincent2004@users.noreply.github.com>
2022-09-06 09:30:54 +0800

    Create Student.java

---
+++ exam_1/Student.java
@@ -0,0 +1,41 @@
+public class Student {
+    // Properties
+    String surname;
+    String firstName;
+    char middleInitial;
+    String dateOfBirth;
+    String studentNumber;
+    String studentEmailAddress;
+    boolean IAmAwesome;
+    
+    // Method
+    
+    public void sayMysurname() {
+       System.out.println ("My surname is " + surname);
+    }
+    
+    public void sayMyfirstName() {
+       System.out.println ("My firstname is " + firstName);
+    }
+    
+    public void sayMymiddleInitial() {
+       System.out.println ("My middleInitial is " + middleInitial);
+    }
+    
+    public void sayMydateOfBirth() {
+       System.out.println ("My dateOfBirth is " + dateOfBirth);
+    }
+    
+    public void sayMystudentNumber() {
+       System.out.println ("My studentNumber is " + studentNumber);
+    }
+    
+    public void sayMystudentEmailAddress() {
+       System.out.println ("My EmailAddress is " + studentEmailAddress);
+    }
+    
+    public void amIAwesome() {
+       System.out.println (" IAmAwesome  " + IAmAwesome);
+    }
+    
+}
